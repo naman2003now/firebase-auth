51c37d04b1628d0bc016368ce99cf772936fb834
naman2003now <a6.namanagrawal@gmail.com>
2022-06-27 17:18:56 +0530

    Done?

--- src/App.js
+++ src/App.js
@@ -4,28 +4,34 @@ import Login from "./components/login";
 import { useState } from "react";
 import { onAuthStateChanged, signOut } from "firebase/auth";
 import { auth } from "./firebase-config";
-import { axios } from "axios";
+import axios from "axios";
 
 function App() {
   const [user, setUser] = useState("");
+  const [secret, setSecret] = useState("");
   onAuthStateChanged(auth, async (currentUser) => {
     setUser(currentUser);
-    const response = await axios.get({
-      url: "localhost:6969/",
-    });
-    console.log(response);
   });
 
   const logout = async () => {
     await signOut(auth);
   };
 
+  const getSecret = async () => {
+    const response = await axios.get("http://localhost:6969/", {
+      headers: { Authorization: "Bearer " + user.accessToken },
+    });
+    console.log(response);
+    setSecret(response.data[0].secret);
+  };
   return (
     <div className="App">
       <Registration />
       <Login />
       <h1> You are {user ? user.email : "Nobody"} </h1>
       <button onClick={logout}>Sign Out</button>
+      <button onClick={getSecret}>Secret</button>
+      <p>{secret}</p>
     </div>
   );
 }
