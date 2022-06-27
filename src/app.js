6f84445b85ff842349955ec25ec0c14e6bd95981
naman2003now <a6.namanagrawal@gmail.com>
2022-06-27 17:18:09 +0530

    Done?

--- src/app.js
+++ src/app.js
@@ -2,16 +2,24 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 const PORT = process.env.PORT || 6969;
+const admin = require("./config/service-account.js");
+
+const users = [
+  {
+    email: "naman2003now@gmail.com",
+    secret: "Mereko ghar jana hai!!",
+  },
+  { email: "somerandomemail@gmail.com", secret: "Random secret!" },
+];
 
 app.use(express.json());
 app.use(cors());
 
-app.get("/", (req, res) => {
-  res.send("You are here!");
-});
-
-app.post("/", (req, res) => {
-  res.send(req.user_token);
+app.get("/", async (req, res) => {
+  const token = req.headers.authorization.split(" ")[1];
+  const user = await admin.auth().verifyIdToken(token);
+  console.log(user.email);
+  res.send(users.filter((u) => u.email == user.email));
 });
 
 app.listen(PORT, () => {
